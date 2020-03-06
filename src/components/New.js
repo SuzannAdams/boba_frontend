1d4f6f40c45b17f1427d3b16d0459edeaf71a4fb
muezzinsarwar <muezzinsarwar17@gmail.com>
2020-03-06 15:41:39 -0600

    Created POST functionality

--- src/components/New.js
+++ src/components/New.js
@@ -4,50 +4,50 @@ import axios from 'axios';
 import { APIURL } from '../config';
 
 class New extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      meet: {
-        name: '',
-        place: '',
-        time: '',
-        topic: '',
-        photo_url: ''
-      }
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			meet: {
+				name: '',
+				place: '',
+				time: '',
+				topic: '',
+				photo_url: ''
+			}
+		};
+	}
 
-  handleChange = evt => {
-    evt.persist();
-    this.setState({
-      meet: {
-        ...this.state.meet,
-        [evt.target.name]: evt.target.value
-      }
-    });
-  };
+	handleChange = evt => {
+		evt.persist();
+		this.setState({
+			meet: {
+				...this.state.meet,
+				[evt.target.name]: evt.target.value
+			}
+		});
+	};
 
-  handleSubmit = event => {
-    event.preventDefault();
-    axios
-      .post(`${APIURL}`)
-      .then(function(response) {
-        console.log(response);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-  };
-  render() {
-    return (
-      <div>
-        <div>New Post</div>
-        <div>
-          <Form />
-        </div>
-      </div>
-    );
-  }
+	handleSubmit = event => {
+		event.preventDefault();
+		axios
+			.post(`${APIURL}/meets/`, this.state.meet)
+			.then(response => console.log(response))
+			.catch(error => console.log(error));
+	};
+	render() {
+		return (
+			<div>
+				<div>New Post</div>
+				<div>
+					<Form
+						meet={this.state.meet}
+						handleChange={this.handleChange}
+						handleSubmit={this.handleSubmit}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default New;

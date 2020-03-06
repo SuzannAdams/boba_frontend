22f1445fc24c9ff263e6f4f5f7a61dbb16d6cf50
muezzinsarwar <muezzinsarwar17@gmail.com>
2020-03-06 16:33:32 -0600

    Created Edit and full CRUD functionality

--- src/App.js
+++ src/App.js
@@ -3,44 +3,49 @@ import './App.css';
 import Home from './components/Home';
 import Meet from './components/Meet';
 import New from './components/New';
+import Edit from './components/Edit';
 import { Link, Switch, Route } from 'react-router-dom';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      meetPageId: null
-    };
-  }
-  setMeetId = id => {
-    this.setState({ meetPageId: id });
-  };
-  render() {
-    return (
-      <div>
-        <nav>
-          <Link to="/">
-            <h1>Boba Clique</h1>
-          </Link>
-        </nav>
-        <main>
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={() => <Home setMeetId={this.setMeetId} />}
-            />
-            <Route
-              exact
-              path="/meet/:id"
-              render={() => <Meet meetId={this.state.meetPageId} />}
-            />
-            <Route exact path="/new" render={() => <New />} />
-          </Switch>
-        </main>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			meetPageId: null
+		};
+	}
+	setMeetId = id => {
+		this.setState({ meetPageId: id });
+	};
+	render() {
+		return (
+			<div>
+				<nav>
+					<Link to="/">
+						<h1>Boba Clique</h1>
+					</Link>
+				</nav>
+				<main>
+					<Switch>
+						<Route
+							exact
+							path="/"
+							render={() => <Home setMeetId={this.setMeetId} />}
+						/>
+						<Route
+							exact
+							path="/meet/:id"
+							render={() => <Meet meetId={this.state.meetPageId} />}
+						/>
+						<Route
+							path="/meet/:id/edit"
+							render={() => <Edit meetId={this.state.meetPageId} />}
+						/>
+						<Route exact path="/new" render={() => <New />} />
+					</Switch>
+				</main>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/Edit.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import Form from './Form';
+import axios from 'axios';
+import { APIURL } from '../config';
+import { Redirect } from 'react-router-dom';
+
+class Edit extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			meet: {
+				name: '',
+				place: '',
+				time: '',
+				topic: '',
+				photo_url: ''
+			},
+			deleted: false
+		};
+	}
+	componentDidMount() {
+		axios
+			.get(`${APIURL}/meets/${this.props.meetId}`)
+			.then(res => this.setState({ meet: res.data }));
+	}
+	handleChange = evt => {
+		evt.persist();
+		this.setState({
+			meet: {
+				...this.state.meet,
+				[evt.target.name]: evt.target.value
+			}
+		});
+	};
+	handleSubmit = event => {
+		event.preventDefault();
+		axios
+			.put(`${APIURL}/meets/${this.props.meetId}`, this.state.meet)
+			.then(response => console.log(response))
+			.catch(error => console.log(error));
+	};
+	handleDelete = evt => {
+		axios
+			.delete(`${APIURL}/meets/${this.props.meetId}`)
+			.then(this.setState({ deleted: true }));
+	};
+	render() {
+		if (this.state.deleted) {
+			return <Redirect to="/" />;
+		}
+		return (
+			<div>
+				<div>Edit Post</div>
+				<div>
+					<Form
+						meet={this.state.meet}
+						handleChange={this.handleChange}
+						handleSubmit={this.handleSubmit}
+					/>
+				</div>
+				<button className="delete-button" onClick={this.handleDelete}>
+					Delete Post
+				</button>
+			</div>
+		);
+	}
+}
+
+export default Edit;

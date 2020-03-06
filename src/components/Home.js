651139a98e4a33be3b0b18a85c6ef85e7dedbd34
SuzannAdams <suzannyee@yahoo.com>
2020-03-06 11:33:56 -0800

    updated img to src

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,38 +4,38 @@ import { Link } from 'react-router-dom';
 
 let posting;
 class Home extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			posts: []
-		};
-	}
-	componentDidMount() {
-		const url = 'http://localhost:8000/meets/';
-		axios.get(url).then(res => this.setState({ posts: res.data }));
-	}
-	render() {
-		let posts = this.state.posts;
-		if (posts !== null) {
-			posting = posts.map(meet => {
-				return (
-					<div key={meet.id}>
-						<Link
-							onClick={() => this.props.setMeetId(meet.id)}
-							to={`/meet/${meet.id}`}
-						>
-							<h3>{meet.name}</h3>
-						</Link>
-						<h4>{meet.place}</h4>
-						<h4>{meet.time}</h4>
-						<h4>{meet.topic}</h4>
-						<img href={meet.photo_url} alt={meet.place} />
-					</div>
-				);
-			});
-		}
-		return <div>{posting}</div>;
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+  componentDidMount() {
+    const url = 'http://localhost:8000/meets/';
+    axios.get(url).then(res => this.setState({ posts: res.data }));
+  }
+  render() {
+    let posts = this.state.posts;
+    if (posts !== null) {
+      posting = posts.map(meet => {
+        return (
+          <div key={meet.id}>
+            <Link
+              onClick={() => this.props.setMeetId(meet.id)}
+              to={`/meet/${meet.id}`}
+            >
+              <h3>{meet.name}</h3>
+            </Link>
+            <h4>{meet.place}</h4>
+            <h4>{meet.time}</h4>
+            <h4>{meet.topic}</h4>
+            <img src={meet.photo_url} alt={meet.place} />
+          </div>
+        );
+      });
+    }
+    return <div>{posting}</div>;
+  }
 }
 
 export default Home;

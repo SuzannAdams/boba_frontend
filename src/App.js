b26554cdf209849fcfdf79fda8ed9b36f63a5473
SuzannAdams <suzannyee@yahoo.com>
2020-03-09 11:43:35 -0700

    changed name from boba clique to boba hour

--- src/App.js
+++ src/App.js
@@ -5,48 +5,47 @@ import Meet from './components/Meet';
 import New from './components/New';
 import Edit from './components/Edit';
 import { Link, Switch, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			meetPageId: null
-		};
-	}
-	setMeetId = id => {
-		this.setState({ meetPageId: id });
-	};
-	render() {
-		return (
-			<div>
-				<nav>
-					<Link to="/">
-						<h1>Boba Clique</h1>
-					</Link>
-				</nav>
-				<main>
-					<Switch>
-						<Route
-							exact
-							path="/"
-							render={() => <Home setMeetId={this.setMeetId} />}
-						/>
-						<Route
-							exact
-							path="/meet/:id"
-							render={() => <Meet meetId={this.state.meetPageId} />}
-						/>
-						<Route
-							path="/meet/:id/edit"
-							render={() => <Edit meetId={this.state.meetPageId} />}
-						/>
-						<Route exact path="/new" render={() => <New />} />
-					</Switch>
-				</main>
-			</div>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      meetPageId: null
+    };
+  }
+  setMeetId = id => {
+    this.setState({ meetPageId: id });
+  };
+  render() {
+    return (
+      <div>
+        <nav>
+          <Link to="/">
+            <h1>Boba Hour</h1>
+          </Link>
+        </nav>
+        <main>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() => <Home setMeetId={this.setMeetId} />}
+            />
+            <Route
+              exact
+              path="/meet/:id"
+              render={() => <Meet meetId={this.state.meetPageId} />}
+            />
+            <Route
+              path="/meet/:id/edit"
+              render={() => <Edit meetId={this.state.meetPageId} />}
+            />
+            <Route exact path="/new" render={() => <New />} />
+          </Switch>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;

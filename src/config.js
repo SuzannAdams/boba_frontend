f7fbaf337fe18eee48c8c484ad140a94ddc86aad
muezzinsarwar <muezzinsarwar17@gmail.com>
2020-03-09 15:26:22 -0500

    Added heroku backend route

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
 export const APIURL =
-  window.location.hostname === 'localhost' ? 'http://localhost:8000' : null;
+	window.location.hostname === 'localhost'
+		? 'http://localhost:8000'
+		: 'https://bobahour-app.herokuapp.com/';
